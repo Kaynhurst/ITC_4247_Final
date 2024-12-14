be0fe293d03f40bdec9dc5cc6e30810057abc335
Kaynhurst <m.strintzis@acg.edu>
2024-12-14 14:09:46 +0200

    Initial Commit of the web app

---
+++ web_app/rest_api/serializers.py
@@ -0,0 +1,45 @@
+from django.utils.timezone import localtime
+from rest_framework import serializers
+from .models import Tasks
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class AppSerializer(serializers.ModelSerializer):
+
+    formatted_time = serializers.SerializerMethodField()
+    formatted_updated = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Tasks
+        fields = ['id','task','description','completed','formatted_time','formatted_updated','user']
+        
+        extra_kwags = {
+            'task' : {'required' : False},
+            'completed' : {'required' : False},
+            'description' : {'required' : False}
+
+        }
+    
+    def get_formatted_time(self, obj):
+        return localtime(obj.timestamp).strftime('%B %d, %Y at %I:%M %p')
+
+    def get_formatted_updated(self, obj):
+        return localtime(obj.updated).strftime('%B %d, %Y at %I:%M %p')
+    
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    def create(self, user_data):
+        user = UserModel.objects.create_user(
+            username= user_data['username'],
+            password = user_data['password']) 
+        
+        return user
+    
+    class Meta :
+        model = UserModel
+        fields = ('id','username','password')
